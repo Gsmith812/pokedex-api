086fae6037dfb22a20d4c8895f9494c8d2ba4864
George Smith <georgesmith812@gmail.com>
2020-11-14 14:48:40 -0500

    wired up the functionality for the /pokemon endpoint to retrieve a list from pokedex.json filtered by query params

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
-console.log(process.env.API_TOKEN);
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
 app.use(morgan('dev'));
+
+app.use(function validateBearerToken (req, res, next) {
+    const authToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN;
+    console.log('validate bearer token middleware')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' });
+    }
+    //move to next middleware
+    next();
+});
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
@@ -13,7 +25,35 @@ function handleGetTypes(req, res) {
     res.json(validTypes);
 }
 
+
+
 app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+    const { type, name } = req.query;
+    //validate type
+    if(type) {
+        if(!validTypes.includes(type)) {
+            return res.status(400).json({error: `Type must match one of these: ${validTypes.join(', ')}`})
+        }
+    }
+    if(type && name) {
+        let results = POKEDEX.pokemon.filter(pokemon => 
+            pokemon.type.includes(type) && pokemon.name.toLowerCase().includes(name.toLowerCase())
+        );
+        return res.json(results);
+    }
+    if(type && !name) {
+        let results = POKEDEX.pokemon.filter(pokemon => pokemon.type.includes(type));
+        return res.json(results);
+    }
+    if(!type && name) {
+        let results = POKEDEX.pokemon.filter(pokemon => pokemon.name.toLowerCase().includes(name.toLowerCase()));
+        return res.json(results);
+    }
+}
+
+app.get('/pokemon', handleGetPokemon);
 
 const PORT = 8000;
 
